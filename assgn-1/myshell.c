71a9c3646924db3eff3da6144304bd7a3c437705
Lior Peled <liorpeled@Liors-MacBook-Pro.local>
2019-04-20 15:33:35 -0700

    added a feature that returns an error if file not found, needs to fix it for redirecting and started the piping function, added symbols to indicate shell is working. made a feature that continues the cycle if pressed enter - command is null

--- assgn-1/myshell.c
+++ assgn-1/myshell.c
@@ -9,168 +9,189 @@
 #include <fcntl.h>  //  need to include for using open()
 extern char **getcommand();
 
-
-/*>
- 
- args = [ls, >, filename]
- tmp_args = [ls, -la]
- 
- close(stdout)
- dup(fd)
- execvp(tmp_args)
- */
-
-
-int Redirect(char **args){
-    int i=0;
-    char ** temp_args;
-    temp_args =  (char **)malloc(0* sizeof(char)); // error
+int Piping(char **args){
+    int numPipes=0;
+    for(i = 0; args[i] != NULL; i++) {
+        if(!strcmp(args[i],"|") )
+        {
+            numPipes++;
+        }
+        
+        
+    }
     
-    int file_desc;
-    for(i = 0; args[i] != NULL; i++) { // no need for this, can do it within for loop in main
-        //can add memory to array herem, then make it equal to args[i] until if statemtn execute, at the end i can use free
+    int Redirect(char **args){
+        int i=0;
+        char ** temp_args =  (char **)malloc(0* sizeof(char)); // error
+        pid_t childPid;
+        int file_desc;
         
-        if(!strcmp(args[i],"<") )
-        {
+        for(i = 0; args[i] != NULL; i++) { // no need for this, can do it within for loop in main
+            //can add memory to array herem, then make it equal to args[i] until if statemtn execute, at the end i can use free
             
-            childPid =fork();
             
-            if(childPid==0)
+            if(!strcmp(args[i],"<") )
             {
                 
-                file_desc = open(args[i+1],O_RDONLY, 0666  );
-                close(STDIN_FILENO);
-                dup(file_desc);
-                close(file_desc);
-                execvp(temp_args[0],temp_args);
-            }else
-            {
-                waitpid(childPid, (void *)0,0);
+                childPid =fork();
+                
+                if(childPid==0)
+                {
+                    
+                    file_desc = open(args[i+1],O_RDONLY, 0666  );
+                    close(STDIN_FILENO);
+                    dup(file_desc);
+                    close(file_desc);
+                    execvp(temp_args[0],temp_args);
+                    // printf("%s\n", "no such command exist" );
+                    exit(errno);
+                }else
+                {
+                    waitpid(childPid, (void *)0,0);
+                }
+                free(temp_args);
+                return 0;
+                
             }
-            free(temp_args);
-            return 0;
+            
+            if(!strcmp(args[i],">") && !strcmp(args[i+1],">"))
+            {
+                
+                childPid =fork();
+                
+                if(childPid==0)
+                {
+                    
+                    file_desc = open(args[i+2], O_CREAT | O_WRONLY | O_APPEND , 0666  );
+                    close(STDOUT_FILENO);
+                    dup(file_desc);
+                    close(file_desc);
+                    execvp(temp_args[0],temp_args);
+                    printf("%s\n", "no such command exist" );
+                    exit(errno);
+                }else
+                {
+                    waitpid(childPid, (void *)0,0);
+                }
+                free(temp_args);
+                return 0;
+                
+            }
+            if(!strcmp(args[i],">"))
+            {
+                
+                childPid =fork();
+                
+                if(childPid==0)
+                {
+                    
+                    file_desc = open(args[i+1], O_CREAT | O_WRONLY | O_TRUNC , 0666  );
+                    close(STDOUT_FILENO);
+                    dup(file_desc);
+                    close(file_desc);
+                    execvp(temp_args[0],temp_args);
+                    printf("%s\n", "no such command exist" );
+                    exit(errno);
+                }else
+                {
+                    waitpid(childPid, (void *)0,0);
+                }
+                //At the end the file has to be closed:
+                
+                
+            }
+            temp_args =  (char **)realloc(temp_args, i+1);
+            temp_args[i] = args[i];
+            //printf("%s\n", temp_args[i]);
+            
             
         }
         
-        if(!strcmp(args[i],">") && !strcmp(args[i+1],">"))
-        {
-            
-            childPid =fork();
-            
-            if(childPid==0)
-            {
-                
-                file_desc = open(args[i+2], O_CREAT | O_WRONLY | O_APPEND , 0666  );
-                close(STDOUT_FILENO);
-                dup(file_desc);
-                close(file_desc);
-                execvp(temp_args[0],temp_args);
-            }else
-            {
-                waitpid(childPid, (void *)0,0);
-            }
-            free(temp_args);
-            return 0;
-            
-        }
+        free(temp_args);
+        return 0;
+    }
+    int commandsNoArgs(char **args){
         
-        if(!strcmp(args[i],">"))
+        if(args[1]==NULL)
         {
-            //printf("%s\n",args[i+1]);
-            
             pid_t childPid;
             childPid =fork();
-            
+            if(childPid <0)
+            {
+                perror("child process error");
+                exit(1); //check later
+            }
             if(childPid==0)
             {
-                file_desc = open(args[i+1], O_CREAT | O_WRONLY| O_TRUNC , 0666 );
+                execvp(args[0],args); //reading first argument from args
+                printf("%s\n", "no such command exist" );
+                exit(errno);
                 
-                close(STDOUT_FILENO);
-                dup(file_desc);//                    dup2(file_desc,1);
-                close(file_desc);
-                   execvp(temp_args[0],temp_args); // this causes the program to be terminated, fixed with fork();
             }else
             {
-                waitpid(childPid, (void *)0,0);
+                wait(0); //check later
             }
-            //At the end the file has to be closed:
-            
-            
-        }
-        temp_args =  (char **)realloc(temp_args, i+1);
-        temp_args[i] = args[i];
-        printf("%s\n", temp_args[i]);
-        
-        
-    }
-    
-    
-    return 0;
-}
-int commandsNoArgs(char **args){
-    
-    if(args[1]==NULL)
-    {
-        pid_t childPid;
-        childPid =fork();
-        if(childPid <0)
-        {
-            perror("child process error");
-            exit(1); //check later
-        }
-        if(childPid==0)
-        {
-            execvp(args[0],args); //reading first argument from args
-            
-        }else
-        {
-            wait(0); //check later
-        }
-    }
-    
-    return 0;
-    
-}
-int commandsArgs(char **args){
-    
-    if(args[1]!=NULL)
-    {
-        pid_t childPid;
-        childPid =fork();
-        if(childPid <0)
-        {
-            perror("child process error");
-            exit(1); //check later
-        }
-        if(childPid==0)
-        {
-            execvp(args[0],args); //reading first argument from args
-            
-        }else
-        {
-            wait(0); //check later
-        }
-    }
-    return 0;
-}
-
-int main() {
-    int i;
-    char **args;
-    
-    while(1) {
-        args = getcommand();
-        
-        if(strcmp(args[0],"exit") == 0||strcmp(args[0],"Exit")==0){
-            
-            exit(1);
-        }
-        Redirect(args);
-        //commandsNoArgs(args);
-        //commandsArgs(args);
-        for(i = 0; args[i] != NULL; i++) {
-            //printf("Argument %d: %s\n", i, args[i]);
         }
         
+        return 0;
+        
     }
-}
+    int commandsArgs(char **args){
+        
+        if(args[1]!=NULL)
+        {
+            pid_t childPid;
+            childPid =fork();
+            if(childPid <0)
+            {
+                perror("child process error");
+                exit(1); //check later
+            }
+            if(childPid==0)
+            {
+                execvp(args[0],args); //reading first argument from args
+                printf("%s\n", "no such command exist" );
+                exit(errno);
+                
+            }else
+            {
+                wait(0); //check later
+            }
+        }
+        return 0;
+    }
+    
+    int main() {
+        int i;
+        char **args;
+        int rd;
+        while(1) {
+            printf("%s ","#" );
+            args = getcommand();
+            if(args[0]==NULL)
+            {
+                continue;
+            }
+            if(strcmp(args[0],"exit") == 0||strcmp(args[0],"Exit")==0){
+                
+                exit(1);
+            }
+            for(i = 0; args[i] != NULL; i++) {
+                if(!strcmp(args[i],">")||!strcmp(args[i],"<"))
+                {
+                    rd =1;
+                }
+            }
+            if(rd)
+            {
+                Redirect(args);
+                rd=0;
+            }else
+            {
+                commandsNoArgs(args);
+                commandsArgs(args);
+            }
+            
+            
+        }
+    }
